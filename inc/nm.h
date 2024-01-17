e6d063f5bd941d70f6244fc0604b66ddb3e2b3af
Naerhy <naerhy@protonmail.com>
2024-01-17 18:29:43 +0100

    Implement minimal file mapping

--- inc/nm.h
+++ inc/nm.h
@@ -1,11 +1,13 @@
 #ifndef NM_H
 #define NM_H
 
+#include <elf.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 #endif
--- src/nm.c
+++ src/nm.c
@@ -1,6 +1,46 @@
 #include "nm.h"
 
-int main(void)
+void handle_file(char const* filename)
 {
+	int fd;
+	struct stat finfo;
+	void* fmap;
+	Elf64_Ehdr* header;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return ;
+	}
+	if (fstat(fd, &finfo) == -1)
+	{
+		return ;
+	}
+	fmap = mmap(NULL, finfo.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+	close(fd);
+	if (fmap == MAP_FAILED)
+	{
+		return ;
+	}
+	printf("file %s of %zu bytes has been mapped\n", filename, finfo.st_size);
+	header = (Elf64_Ehdr*)fmap;
+	for (size_t i = 0; i < 4; i++)
+		printf("%X\n", header->e_ident[i]);
+	munmap(fmap, finfo.st_size);
+}
+
+int main(int argc, char const* const* argv)
+{
+	if (argc == 1)
+	{
+		handle_file("a.out");
+	}
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			handle_file(*(argv + i));
+		}
+	}
 	return 0;
 }
