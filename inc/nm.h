6140805ebabb23834574a7f671ad1abd9b51c542
Naerhy <naerhy@protonmail.com>
2024-01-25 11:48:06 +0100

    Implement symbols printing without validation

--- inc/nm.h
+++ inc/nm.h
@@ -1,13 +1,27 @@
 #ifndef NM_H
 #define NM_H
 
+/*
+ * check if size of file is >= 5
+ * verify ELF header + 32 or 64 bits
+ * if valid -> verify if size >= structure size
+ * parse structure (if needed)
+ * loop on symbols?
+ *
+ * remove all symbols which are st_info = FILE or st_shndx = ABS
+ */
+
 #include <elf.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
+
+// TODO: remove
+#include <string.h>
 
 #endif
--- src/nm.c
+++ src/nm.c
@@ -1,45 +1,116 @@
 #include "nm.h"
 
-void handle_file(char const* filename)
+static unsigned char check_intpr(unsigned char const* intpr, off_t fsize)
+{
+	if (fsize < EI_NIDENT || intpr[EI_MAG0] != ELFMAG0 || intpr[EI_MAG1] != ELFMAG1
+			|| intpr[EI_MAG2] != ELFMAG2 || intpr[EI_MAG3] != ELFMAG3)
+		return ELFCLASSNONE;
+	return intpr[EI_CLASS];
+}
+
+static void handle_32bits(Elf32_Ehdr* header)
+{
+	Elf32_Shdr* sht;
+	unsigned char* shtname;
+	Elf32_Shdr* symtab;
+	unsigned char* strtab;
+	Elf32_Sym* symbols;
+	Elf32_Sym* curr_symbol;
+
+	if (header->e_ehsize != 52 || !header->e_shoff)
+		return ;
+	sht = (Elf32_Shdr*)((unsigned char*)header + header->e_shoff);
+	shtname = (unsigned char*)header + (sht + header->e_shstrndx)->sh_offset;
+	for (uint16_t i = 0; i < header->e_shnum; i++)
+	{
+		if (!strcmp(".symtab", (char*)shtname + (sht + i)->sh_name))
+		{
+			symtab = sht + i;
+			strtab = (unsigned char*)header + (sht + symtab->sh_link)->sh_offset;
+			symbols = (Elf32_Sym*)((unsigned char*)header + symtab->sh_offset);
+			for (uint64_t j = 0; j < symtab->sh_size; j += symtab->sh_entsize)
+			{
+				curr_symbol = symbols + j / symtab->sh_entsize;
+				if (curr_symbol->st_info != STT_FILE && curr_symbol->st_name)
+					printf("%s\n", strtab + curr_symbol->st_name);
+			}
+		}
+	}
+}
+
+static void handle_64bits(Elf64_Ehdr* header)
+{
+	Elf64_Shdr* sht;
+	unsigned char* shtname;
+	Elf64_Shdr* symtab;
+	unsigned char* strtab;
+	Elf64_Sym* symbols;
+	Elf64_Sym* curr_symbol;
+
+	if (header->e_ehsize != 64 || !header->e_shoff)
+		return ;
+	sht = (Elf64_Shdr*)((unsigned char*)header + header->e_shoff);
+	shtname = (unsigned char*)header + (sht + header->e_shstrndx)->sh_offset;
+	for (uint16_t i = 0; i < header->e_shnum; i++)
+	{
+		if (!strcmp(".symtab", (char*)shtname + (sht + i)->sh_name))
+		{
+			symtab = sht + i;
+			strtab = (unsigned char*)header + (sht + symtab->sh_link)->sh_offset;
+			symbols = (Elf64_Sym*)((unsigned char*)header + symtab->sh_offset);
+			for (uint64_t j = 0; j < symtab->sh_size; j += symtab->sh_entsize)
+			{
+				curr_symbol = symbols + j / symtab->sh_entsize;
+				if (curr_symbol->st_info != STT_FILE && curr_symbol->st_name)
+					printf("%s\n", strtab + curr_symbol->st_name);
+			}
+		}
+	}
+}
+
+static int handle_file(char const* filename)
 {
 	int fd;
 	struct stat finfo;
 	void* fmap;
-	Elf64_Ehdr* header;
+	unsigned char class;
 
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		return ;
-	}
-	if (fstat(fd, &finfo) == -1)
-	{
-		return ;
-	}
+	if (fd == -1 || fstat(fd, &finfo) == -1)
+		return 0;
 	fmap = mmap(NULL, finfo.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
 	close(fd);
 	if (fmap == MAP_FAILED)
+		return 0;
+	class = check_intpr(fmap, finfo.st_size);
+	if (class == ELFCLASS32 && finfo.st_size >= 52)
 	{
-		return ;
+		handle_32bits(fmap);
 	}
-	printf("file %s of %zu bytes has been mapped\n", filename, finfo.st_size);
-	header = (Elf64_Ehdr*)fmap;
-	for (size_t i = 0; i < 4; i++)
-		printf("%X\n", header->e_ident[i]);
+	else if (class == ELFCLASS64 && finfo.st_size >= 64)
+	{
+		handle_64bits(fmap);
+	}
+	else
+		// TODO: munmap if error
+		return 0;
 	munmap(fmap, finfo.st_size);
+	return 1;
 }
 
 int main(int argc, char const* const* argv)
 {
 	if (argc == 1)
 	{
-		handle_file("a.out");
+		if (!handle_file("a.out"))
+			perror("nm");
 	}
 	else
 	{
 		for (int i = 1; i < argc; i++)
 		{
-			handle_file(*(argv + i));
+			if (!handle_file(*(argv + i)))
+				perror("nm");
 		}
 	}
 	return 0;
