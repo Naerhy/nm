1c506b5824166d4f04e11238109138b818b82fee
Naerhy <naerhy@protonmail.com>
2024-01-29 19:21:55 +0100

    Implement symbols type parsing

--- src/nm.c
+++ src/nm.c
@@ -11,28 +11,83 @@ static unsigned char check_intpr(unsigned char const* intpr, off_t fsize)
 static void handle_32bits(Elf32_Ehdr* header)
 {
 	Elf32_Shdr* sht;
-	unsigned char* shtname;
-	Elf32_Shdr* symtab;
+	unsigned char* shstrtab;
+	Elf32_Shdr* sh_symtab;
+	Elf32_Sym* symtab;
 	unsigned char* strtab;
-	Elf32_Sym* symbols;
-	Elf32_Sym* curr_symbol;
+	Elf32_Sym* symbol;
 
 	if (header->e_ehsize != 52 || !header->e_shoff)
 		return ;
 	sht = (Elf32_Shdr*)((unsigned char*)header + header->e_shoff);
-	shtname = (unsigned char*)header + (sht + header->e_shstrndx)->sh_offset;
+	shstrtab = (unsigned char*)header + (sht + header->e_shstrndx)->sh_offset;
 	for (uint16_t i = 0; i < header->e_shnum; i++)
 	{
-		if (!strcmp(".symtab", (char*)shtname + (sht + i)->sh_name))
+		if (!strcmp(".symtab", (char*)shstrtab + (sht + i)->sh_name))
 		{
-			symtab = sht + i;
-			strtab = (unsigned char*)header + (sht + symtab->sh_link)->sh_offset;
-			symbols = (Elf32_Sym*)((unsigned char*)header + symtab->sh_offset);
-			for (uint64_t j = 0; j < symtab->sh_size; j += symtab->sh_entsize)
+			sh_symtab = sht + i;
+			strtab = (unsigned char*)header + (sht + sh_symtab->sh_link)->sh_offset;
+			symtab = (Elf32_Sym*)((unsigned char*)header + sh_symtab->sh_offset);
+			for (uint32_t j = sh_symtab->sh_entsize; j < sh_symtab->sh_size; j += sh_symtab->sh_entsize)
 			{
-				curr_symbol = symbols + j / symtab->sh_entsize;
-				if (curr_symbol->st_info != STT_FILE && curr_symbol->st_name)
-					printf("%s\n", strtab + curr_symbol->st_name);
+				symbol = symtab + j / sh_symtab->sh_entsize;
+				if (ELF32_ST_TYPE(symbol->st_info) != STT_FILE && ELF32_ST_TYPE(symbol->st_info) != STT_SECTION)
+				{
+
+					// print value
+					if (symbol->st_shndx == SHN_UNDEF)
+						printf("%8s", "");
+					else
+						printf("%08x", symbol->st_value);
+					printf(" ");
+
+					// print type
+					if (ELF32_ST_TYPE(symbol->st_info) == STT_GNU_IFUNC)
+						printf("i");
+					else if (ELF32_ST_BIND(symbol->st_info) == STB_GNU_UNIQUE)
+						printf("u");
+					else if (ELF32_ST_BIND(symbol->st_info) == STB_WEAK)
+					{
+						if (ELF32_ST_TYPE(symbol->st_info) == STT_OBJECT)
+							printf(symbol->st_shndx != SHN_UNDEF ? "V" : "v");
+						else
+						{
+							printf(symbol->st_shndx != SHN_UNDEF ? "W" : "w");
+						}
+					}
+					else if (symbol->st_shndx == SHN_ABS)
+						printf("A");
+					else if (symbol->st_shndx == SHN_COMMON)
+						printf("C");
+					else if (symbol->st_shndx == SHN_UNDEF)
+						printf("U");
+					else if (!strcmp((char*)shstrtab + (sht + symbol->st_shndx)->sh_name, ".debug")
+							|| !strcmp((char*)shstrtab + (sht + symbol->st_shndx)->sh_name, ".line"))
+						printf("N");
+					else if ((sht + symbol->st_shndx)->sh_flags & SHF_EXECINSTR)
+						printf(ELF32_ST_BIND(symbol->st_info) != STB_LOCAL ? "T" : "t");
+					else if ((sht + symbol->st_shndx)->sh_flags & SHF_WRITE
+							&& (sht + symbol->st_shndx)->sh_flags & SHF_ALLOC)
+					{
+						if ((sht + symbol->st_shndx)->sh_type == SHT_NOBITS)
+							printf(ELF32_ST_BIND(symbol->st_info) != STB_LOCAL ? "B" : "b");
+						else
+							printf(ELF32_ST_BIND(symbol->st_info) != STB_LOCAL ? "D" : "d");
+					}
+					else if (!((sht + symbol->st_shndx)->sh_flags & SHF_WRITE))
+						printf(ELF32_ST_BIND(symbol->st_info) != STB_LOCAL ? "R" : "r");
+					else
+						printf("?");
+					printf(" ");
+
+					// print name
+					if (symbol->st_name)
+						printf("%s", strtab + symbol->st_name);
+					else
+						printf("(null)");
+					printf("\n");
+
+				}
 			}
 		}
 	}
@@ -41,28 +96,83 @@ static void handle_32bits(Elf32_Ehdr* header)
 static void handle_64bits(Elf64_Ehdr* header)
 {
 	Elf64_Shdr* sht;
-	unsigned char* shtname;
-	Elf64_Shdr* symtab;
+	unsigned char* shstrtab;
+	Elf64_Shdr* sh_symtab;
+	Elf64_Sym* symtab;
 	unsigned char* strtab;
-	Elf64_Sym* symbols;
-	Elf64_Sym* curr_symbol;
+	Elf64_Sym* symbol;
 
 	if (header->e_ehsize != 64 || !header->e_shoff)
 		return ;
 	sht = (Elf64_Shdr*)((unsigned char*)header + header->e_shoff);
-	shtname = (unsigned char*)header + (sht + header->e_shstrndx)->sh_offset;
+	shstrtab = (unsigned char*)header + (sht + header->e_shstrndx)->sh_offset;
 	for (uint16_t i = 0; i < header->e_shnum; i++)
 	{
-		if (!strcmp(".symtab", (char*)shtname + (sht + i)->sh_name))
+		if (!strcmp(".symtab", (char*)shstrtab + (sht + i)->sh_name))
 		{
-			symtab = sht + i;
-			strtab = (unsigned char*)header + (sht + symtab->sh_link)->sh_offset;
-			symbols = (Elf64_Sym*)((unsigned char*)header + symtab->sh_offset);
-			for (uint64_t j = 0; j < symtab->sh_size; j += symtab->sh_entsize)
+			sh_symtab = sht + i;
+			strtab = (unsigned char*)header + (sht + sh_symtab->sh_link)->sh_offset;
+			symtab = (Elf64_Sym*)((unsigned char*)header + sh_symtab->sh_offset);
+			for (uint64_t j = sh_symtab->sh_entsize; j < sh_symtab->sh_size; j += sh_symtab->sh_entsize)
 			{
-				curr_symbol = symbols + j / symtab->sh_entsize;
-				if (curr_symbol->st_info != STT_FILE && curr_symbol->st_name)
-					printf("%s\n", strtab + curr_symbol->st_name);
+				symbol = symtab + j / sh_symtab->sh_entsize;
+				if (ELF64_ST_TYPE(symbol->st_info) != STT_FILE && ELF64_ST_TYPE(symbol->st_info) != STT_SECTION)
+				{
+
+					// print value
+					if (symbol->st_shndx == SHN_UNDEF)
+						printf("%16s", "");
+					else
+						printf("%016lx", symbol->st_value);
+					printf(" ");
+
+					// print type
+					if (ELF64_ST_TYPE(symbol->st_info) == STT_GNU_IFUNC)
+						printf("i");
+					else if (ELF64_ST_BIND(symbol->st_info) == STB_GNU_UNIQUE)
+						printf("u");
+					else if (ELF64_ST_BIND(symbol->st_info) == STB_WEAK)
+					{
+						if (ELF64_ST_TYPE(symbol->st_info) == STT_OBJECT)
+							printf(symbol->st_shndx != SHN_UNDEF ? "V" : "v");
+						else
+						{
+							printf(symbol->st_shndx != SHN_UNDEF ? "W" : "w");
+						}
+					}
+					else if (symbol->st_shndx == SHN_ABS)
+						printf("A");
+					else if (symbol->st_shndx == SHN_COMMON)
+						printf("C");
+					else if (symbol->st_shndx == SHN_UNDEF)
+						printf("U");
+					else if (!strcmp((char*)shstrtab + (sht + symbol->st_shndx)->sh_name, ".debug")
+							|| !strcmp((char*)shstrtab + (sht + symbol->st_shndx)->sh_name, ".line"))
+						printf("N");
+					else if ((sht + symbol->st_shndx)->sh_flags & SHF_EXECINSTR)
+						printf(ELF64_ST_BIND(symbol->st_info) != STB_LOCAL ? "T" : "t");
+					else if ((sht + symbol->st_shndx)->sh_flags & SHF_WRITE
+							&& (sht + symbol->st_shndx)->sh_flags & SHF_ALLOC)
+					{
+						if ((sht + symbol->st_shndx)->sh_type == SHT_NOBITS)
+							printf(ELF64_ST_BIND(symbol->st_info) != STB_LOCAL ? "B" : "b");
+						else
+							printf(ELF64_ST_BIND(symbol->st_info) != STB_LOCAL ? "D" : "d");
+					}
+					else if (!((sht + symbol->st_shndx)->sh_flags & SHF_WRITE))
+						printf(ELF64_ST_BIND(symbol->st_info) != STB_LOCAL ? "R" : "r");
+					else
+						printf("?");
+					printf(" ");
+
+					// print name
+					if (symbol->st_name)
+						printf("%s", strtab + symbol->st_name);
+					else
+						printf("(null)");
+					printf("\n");
+
+				}
 			}
 		}
 	}
